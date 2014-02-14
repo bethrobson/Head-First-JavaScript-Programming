a30c0f0fb294b65fcd5232fac9ca7b108083cff5
Elisabeth Robson <bethrobson@gmail.com>
2014-02-13 17:41:54 -0800

    updates to chapter 2 code

--- chapter2/battleship-final.js
+++ chapter2/battleship-final.js
@@ -1,37 +1,30 @@
-// Ready Bake Code
-function logToBrowser(message) {
-    var results = document.getElementById("results");
-    results.innerHTML = results.innerHTML + "<br>" + message;
-}
-
 var randomLoc = Math.floor(Math.random() * 5);
 var location1 = randomLoc;
 var location2 = location1 + 1;
-var location3 = location2 + 1;
-var guess; 
+var location3 = location1 + 2;
+var guess;
 var hits = 0;
 var guesses = 0;
 var isSunk = false;
 
-while (isSunk == false) { 
-    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
-    if (guess < 0 || guess > 6) {  
-       logToBrowser("Please enter a valid cell number!");
+while (isSunk == false) {
+	guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+	if (guess < 0 || guess > 6) {
+		alert("Please enter a valid cell number!");
 	} else {
-      guesses = guesses + 1;
-
-      if (guess == location1 || guess == location2 || guess == location3) {
-          logToBrowser("HIT!");
-          hits = hits + 1;
-          if (hits == 3) {
-            isSunk = true;
-            logToBrowser("You sank my battleship!");
-          }
-      } else {
-         logToBrowser("MISS");
-      }
-    }
-} 
+		guesses = guesses + 1;
+		if (guess == location1 || guess == location2 || guess == location3) {
+			alert("HIT!");
+			hits = hits + 1;
+			if (hits == 3) {
+				isSunk = true;
+				alert("You sank my battleship!");
+			}
+		} else {
+			alert("MISS");
+		}
+	}
+}
 var stats = "You took " + guesses + " guesses to sink the battleship, " +
-               "which means your shooting accuracy was " + (3/guesses);
-logToBrowser(stats);
+            "which means your shooting accuracy was " + (3/guesses);
+alert(stats);
--- chapter2/battleship.js
+++ chapter2/battleship.js
@@ -1,43 +1,29 @@
-// Ready Bake Code
-function logToBrowser(message) {
-    var results = document.getElementById("results");
-    results.innerHTML = results.innerHTML + "<br>" + message;
-	console.log(message);
-	alert(message);
-}
-
-function getUserInput() {
-	var input = prompt("Ready, aim, fire! (enter a number from 0-6):");
-	return input;
-}
-
 var location1 = 3;
 var location2 = 4;
 var location3 = 5;
-var guess; 
+var guess;
 var hits = 0;
 var guesses = 0;
 var isSunk = false;
 
-while (isSunk == false) { 
-    guess = getUserInput();
-    if (guess < 0 || guess > 6) {  
-       logToBrowser("Please enter a valid cell number!");
+while (isSunk == false) {
+	guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+	if (guess < 0 || guess > 6) {
+		alert("Please enter a valid cell number!");
 	} else {
-      guesses = guesses + 1;
-
-      if (guess == location1 || guess == location2 || guess == location3) {
-          logToBrowser("HIT!");
-          hits = hits + 1;
-          if (hits == 3) {
-            isSunk = true;
-            logToBrowser("You sank my battleship!");
-          }
-      } else {
-         logToBrowser("MISS");
-      }
-    }
-} 
+		guesses = guesses + 1;
+		if (guess == location1 || guess == location2 || guess == location3) {
+			alert("HIT!");
+			hits = hits + 1;
+			if (hits == 3) {
+				isSunk = true;
+				alert("You sank my battleship!");
+			}
+		} else {
+			alert("MISS");
+		}
+	}
+}
 var stats = "You took " + guesses + " guesses to sink the battleship, " +
-               "which means your shooting accuracy was " + (3/guesses);
-logToBrowser(stats);
+            "which means your shooting accuracy was " + (3/guesses);
+alert(stats);
